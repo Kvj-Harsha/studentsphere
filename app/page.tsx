7e3f06490bcbae0a178663d89b6457213c7a7013
Kvj-Harsha <cs23b1034@iiitr.ac.in>
2025-02-02 10:44:51 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const page = () => {
+
+const Navbar = () => {
   return (
     <div>
+      {/* <Navbar/> */}
       Home! lets go!
     </div>
   )
 }
 
-export default page
+export default Navbar
