829962b09f376a2ef291c83c85e57189973c7c71
Y-Santhosh <ad23b1060@iiitr.ac.in>
2025-02-02 12:17:10 +0530

    changes

--- app/components/Hero/page.tsx
+++ app/components/Hero/page.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { HeroHighlight, Highlight } from "..//hero-highlights";
 
 const Hero = () => {
   return (
-    <section>
-        <main className='flex text-center justify-center items-center min-h-screen text-6xl'>
-            Welcome to Student Sphere!
-        </main>
-            {/* cta */}
-        <section>
-            {/* button */}
-        </section>
-    </section>
-  )
-}
+    <HeroHighlight containerClassName="min-h-screen flex flex-col items-center justify-center text-center">
+      <main className="text-6xl font-bold">
+       Welcome to <Highlight>Student Sphere!</Highlight> {/* Only wrap the heading */}
+      </main>
+    </HeroHighlight>
+        
+        // {/* Search and CTA Section */}
+        // <containerClassName="min"
+        // <input
+        //   type="text"
+        //   placeholder="Search..."
+        //   className="mt-20 p-2 border rounded-lg w-64"
+        // />
+        // <div className="mt-4 flex gap-4">
+        //   <a href="/announcements" className="px-4 py-2 bg-blue-500 text-white rounded-lg">
+        //     Announcements
+        //   </a>
+        // </div>
+  );
+};
 
-export default Hero
+export default Hero;
